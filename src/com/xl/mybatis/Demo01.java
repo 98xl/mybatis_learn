253ee69582f6cb6bf50dfa1ec86b79fe6687b69a
98xl <52913615+98xl@users.noreply.github.com>
2019-07-22 16:20:16 +0800

    Update Demo01.java
    第三次更新demo01

--- src/com/xl/mybatis/Demo01.java
+++ src/com/xl/mybatis/Demo01.java
@@ -8,5 +8,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("备注");
+        System.out.println("第三次");
     }
 }
